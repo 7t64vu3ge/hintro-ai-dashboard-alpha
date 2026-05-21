5733604aa2089a9ce33e3d703644a72184a4ff25
7t64vu3ge <timmyojha@gmail.com>
2026-05-21 23:34:37 +0530

    environment update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "https://mock-backend-hintro.vercel.app",
+  baseURL: import.meta.env.VITE_API_URL || "https://mock-backend-hintro.vercel.app",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
